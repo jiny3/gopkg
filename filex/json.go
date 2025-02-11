c837ecb8d4a867aefc903777f499da72ee0c9351
jiny3 <jinyan1419@gmail.com>
2025-02-11 14:55:02 +0800

    🦄 refactor: enhance JSON handling functions for better file management and error handling

--- filex/json.go
+++ filex/json.go
@@ -2,43 +2,55 @@ package filex
 
 import (
 	"encoding/json"
-	"os"
 )
 
-func JsonGet[T any](file string, entrys *[]T) error {
-	openFile, err := os.OpenFile(file, os.O_RDONLY, 0666)
+func JsonGet[T any](file string, entries *[]T) error {
+	f, err := FileOpen(file)
 	if err != nil {
 		return err
 	}
-	defer openFile.Close()
+	defer f.Close()
 
-	decoder := json.NewDecoder(openFile)
-	for {
-		var entry T
-		if err := decoder.Decode(&entry); err != nil {
-			break
-		}
-		*entrys = append(*entrys, entry)
+	// 检查文件是否为空
+	stat, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	if stat.Size() == 0 {
+		*entries = []T{}
+		return nil
 	}
 
+	decoder := json.NewDecoder(f)
+	if err := decoder.Decode(entries); err != nil {
+		return err
+	}
+	return nil
+}
+
+func JsonSet[T any](file string, entries []T) error {
+	err := FileDelete(file)
+	if err != nil {
+		return err
+	}
+	f, err := FileOpen(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	encoder := json.NewEncoder(f)
+	if err := encoder.Encode(entries); err != nil {
+		return err
+	}
 	return nil
 }
 
 func JsonInsert[T any](file string, entry T) error {
-	// T to json
-	jsonData, err := json.Marshal(entry)
-	if err != nil {
+	var entries []T
+	if err := JsonGet(file, &entries); err != nil {
 		return err
 	}
-	// open file
-	openFile, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		return err
-	}
-	defer openFile.Close()
-	// write json
-	if _, err := openFile.Write(jsonData); err != nil {
-		return err
-	}
-	return nil
+	entries = append(entries, entry)
+	return JsonSet(file, entries)
 }
