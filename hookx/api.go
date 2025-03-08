51dc17d5e94af72d9b8df15dbe02402b2f5ff7da
jiny3 <jinyan1419@gmail.com>
2025-03-08 16:35:15 +0800

    🌈 style: rename parameters in Exit and Init functions for clarity

--- hookx/api.go
+++ hookx/api.go
@@ -1,20 +1,21 @@
 package hookx
 
 func init() {
+	// init signal
 	go exitWait()
 }
 
 // add hooks to exitlist,
 // when receive os.Interrupt or syscall.SIGTERM signal, run all hooks
-func Exit(hooks ...func()) {
+func Exit(opts ...func()) {
 	exitListLock.Lock()
 	defer exitListLock.Unlock()
-	exitList = append(exitList, hooks...)
+	exitList = append(exitList, opts...)
 }
 
 // run hooks and ensure only run once,
 // suggest to use in main.init(),
 // input: a list of *func()
-func Init(hooks ...(*func())) {
-	onceRun(hooks)
+func Init(opts ...(*func())) {
+	onceRun(opts)
 }
