e8c7630b247e26e1add3d852cfdff000b9d6f4c4
jiny3 <jinyan1419@gmail.com>
2025-05-25 17:23:36 +0800

    🦄 refactor: rm default log & rm delete old log

--- logx/init.go
+++ logx/init.go
@@ -11,22 +11,12 @@ import (
 // Init global logrus
 func Init(level logrus.Level, path ...string) {
 	logWriters := []io.Writer{os.Stdout}
-	if len(path) == 0 {
-		filex.FileDelete("logs/default.log")
-		w, err := filex.FileOpen("logs/default.log")
+	for _, p := range path {
+		w, err := filex.FileOpen(p)
 		if err != nil {
-			logrus.WithError(err).WithField("path", "logs/default.log").Fatal("open log file failed")
+			logrus.WithError(err).WithField("path", p).Fatal("open log file failed")
 		}
 		logWriters = append(logWriters, w)
-	} else {
-		for _, p := range path {
-			filex.FileDelete(p)
-			w, err := filex.FileOpen(p)
-			if err != nil {
-				logrus.WithError(err).WithField("path", p).Fatal("open log file failed")
-			}
-			logWriters = append(logWriters, w)
-		}
 	}
 	logrus.SetOutput(io.MultiWriter(logWriters...))
 	logrus.SetLevel(level)
